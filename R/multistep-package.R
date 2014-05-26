6c2c27df7b920f825a4e7a35cb71e591de1cfe8e
Sergei Izrailev <sizrailev@collective.com>
2014-05-26 13:04:59 -0400

    package skeleton

---
+++ R/multistep-package.R
@@ -0,0 +1,40 @@
+#-------------------------------------------------------------------------------
+#
+# Package multistep 
+#
+# General description 
+# 
+# Sergei Izrailev, 2014
+#-------------------------------------------------------------------------------
+# Copyright 2011-2014 Collective, Inc.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#-------------------------------------------------------------------------------
+
+#' Utilities for running, timing and logging a multi-step process from R.
+#'
+#' \tabular{ll}{
+#' Package: \tab multistep\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.1\cr
+#' Date: \tab 2014-05-26\cr
+#' License: \tab Apache License, Version 2.0\cr
+#' Author: \tab Sergei Izrailev, Jeremy Stanley\cr
+#' Maintainer: \tab Sergei Izrailev <sizrailev@@collective.com>\cr
+#' LazyLoad: \tab yes\cr
+#' }
+#' 
+#' @name multistep
+#' @docType package
+#' @import cmrutils
+NULL
